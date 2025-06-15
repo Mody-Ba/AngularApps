2c83e9ec9340f397a42ea7805a5a50579d61ec62
Mody <Mody.Ba@uqtr.ca>
2025-06-15 01:23:14 -0400

    integration template bootstrapet config des route vers les pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { ProductsComponent } from './products/products.component';
+import { CustomersComponent } from './customers/customers.component';
+export const routes: Routes = [
+  {path: 'product',component:ProductsComponent},
+  { path: '', redirectTo: 'product', pathMatch: 'full' },
+  {path:'customer',component:CustomersComponent},
+  {path: '', redirectTo: 'customer', pathMatch: 'full'}];
